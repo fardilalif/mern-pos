e14e598121c3e1e91d9b07d53734e38e3f6d8f3d
fardilalif <muhdfardilalif@gmail.com>
2024-02-28 15:56:42 +0800

    Revert "add .env in gitignore"
    This reverts commit 34952cf074783b47417966531e62c8bb5de1ebed.

--- utils/tokenUtils.js
+++ utils/tokenUtils.js
@@ -2,7 +2,7 @@ import jwt from "jsonwebtoken";
 
 export const createJWT = (payload) => {
   const token = jwt.sign(payload, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
+    expiresIn: process.env.EXPIRES_in,
   });
 
   return token;
